a2e1b4727c8f8ce09db8101f8d7ef89232bcf68f
jmroon <36086006+jmroon@users.noreply.github.com>
2021-09-14 22:12:02 -0400

    remove app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const prompt = require("prompt");
-console.log("Hello World!");
-prompt.get(["test"], (err, result) => {
-    console.log("your result: " + result.test);
-});
