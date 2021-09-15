ef2c122117f5ba5b2c41cac2c6e1cf8afea38d39
jmroon <36086006+jmroon@users.noreply.github.com>
2021-09-15 14:31:08 -0400

    initial speed implementation

--- src/app.ts
+++ src/app.ts
@@ -1,60 +1,93 @@
-import fs from "fs";
-import prompts, { PromptObject, Options } from "prompts";
+import fs from 'fs';
+import prompts, { PromptObject, Options } from 'prompts';
+import { WALK_SPEED_CLASSIC, WALK_SPEED_FAST, WALK_SPEED_ORIGINAL, WalkSpeed } from './ga_constants/speed';
 
-const GA_FILE = "GameAssembly.dll";
-const BACKUP_DIR = "./backup_dll";
+const GA_FILE = 'GameAssembly.dll';
+const BACKUP_DIR = './backup_dll';
 const QUESTIONS: PromptObject[] = [
   {
-    type: "select",
-    name: "speed",
-    message: "Please choose a game speed",
+    type: 'select',
+    name: 'speed',
+    message: 'Please choose a game speed. Classic or Fast should result in less stutter while walking.',
     choices: [
       {
-        title: "Classic",
-        description: "The same speed as the original SNES game (60 px/second)",
-        value: "0ad7a33e",
+        title: 'Original',
+        description: 'The original Pixel Remaster speed (80 px/second).',
+        value: WALK_SPEED_ORIGINAL,
       },
       {
-        title: "Fast",
-        description:
-          "1.5x faster than the Pixel Remaster default speed (120 px/second)",
-        value: "0ad7233e",
+        title: 'Classic',
+        description: 'The same speed as the original SNES game (60 px/second).',
+        value: WALK_SPEED_CLASSIC,
+      },
+      {
+        title: 'Fast',
+        description: '1.5x faster than the Pixel Remaster default speed (120 px/second)',
+        value: WALK_SPEED_FAST,
       },
     ],
   },
 ];
 
-const GA_WALK_SPEED_BASE = "8fc2753e";
-
 async function main() {
   if (!fs.existsSync(GA_FILE)) {
-    console.log(
-      "No GameAssembly.dll found. Please ensure you're running this app from the root game directory."
-    );
+    console.log("No GameAssembly.dll found. Please ensure you're running this app from the root game directory.");
     return;
   }
 
   const response = await prompts(QUESTIONS);
+  const walkSpeed = response.speed as WalkSpeed;
 
+  backupDll();
+
+  const file = fs.readFileSync(`./${GA_FILE}`, 'hex');
+
+  try {
+    let patchedFile = replaceSingleOccurence(file, WALK_SPEED_ORIGINAL.base, walkSpeed.base, 'Base Speed');
+
+    walkSpeed.lookupTable.forEach((value, index) => {
+      patchedFile = replaceSingleOccurence(
+        patchedFile,
+        WALK_SPEED_ORIGINAL.lookupTable[index],
+        value,
+        'Lookup Table Speed' // todo: label which speed corresponds to what
+      );
+    });
+
+    fs.writeFileSync(`./${GA_FILE}`, patchedFile, 'hex');
+
+    console.log('Success!');
+  } catch (err: any) {
+    console.log(err);
+    return;
+  }
+}
+
+function backupDll() {
   if (!fs.existsSync(BACKUP_DIR)) {
     fs.mkdirSync(BACKUP_DIR);
   }
-
   fs.copyFileSync(`./${GA_FILE}`, `${BACKUP_DIR}/${GA_FILE}`);
 
-  const file = fs.readFileSync(`./${GA_FILE}`, "hex");
+  // todo: append a filename friendly timestamp or increment to the end of the filename so we don't lose the original
+}
 
-  const patchedFile = file.replace(GA_WALK_SPEED_BASE, response.speed);
+function replaceSingleOccurence(file: string, searchValue: string, replaceValue: string, description = 'Value') {
+  const matches = file.match(new RegExp(searchValue, 'g'));
+  if (!matches || matches.length == 0) {
+    throw Error(`${description}: not found. Ensure you are modifying the original GameAssembly.dll`);
+  } else if (matches.length > 1) {
+    throw Error(`${description}: multiple matches found. Cannot identify which to replace.`);
+  }
 
-  fs.writeFileSync(`./${GA_FILE}`, patchedFile, "hex");
-
-  console.log("Success!");
+  console.log(`${description}: replacing ${searchValue} with ${replaceValue}`);
+  return file.replace(searchValue, replaceValue);
 }
 
 main().then(async (r) => {
   await prompts({
-    type: "text",
-    name: "exit",
-    message: "Press ENTER to exit.",
+    type: 'text',
+    name: 'exit',
+    message: 'Press ENTER to exit.',
   });
 });
---
+++ src/ga_constants/speed.ts
@@ -0,0 +1,34 @@
+export const WALK_SPEED_ORIGINAL: WalkSpeed = {
+  base: '8fc2753e11111111', // 0.24
+  lookupTable: [
+    'c781a400000000008040', // 4.00
+    'c781a40000009a991940', // 2.40
+    'c781a40000006666e63f', // 1.80
+    'c781a40000009a99993f', // 1.20
+  ],
+};
+
+export const WALK_SPEED_CLASSIC: WalkSpeed = {
+  base: '0ad7a33e', // 0.24
+  lookupTable: [
+    'c781a40000004099999a', // 4.80
+    'c781a40000009a991940', // 2.40
+    'c781a40000009a991940', // 2.40
+    'c781a40000009a99993f', // 1.20
+  ],
+};
+
+export const WALK_SPEED_FAST: WalkSpeed = {
+  base: '0ad7233e', // 0.24
+  lookupTable: [
+    'c781a400000066666640', // 3.60
+    'c781a40000009a991940', // 2.40
+    'c781a40000009a99993f', // 1.20
+    'c781a40000009a99993f', // 1.20
+  ],
+};
+
+export interface WalkSpeed {
+  base: string;
+  lookupTable: Array<string>;
+}
